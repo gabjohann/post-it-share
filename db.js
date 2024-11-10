787a8f5b28a27146e59f9133140a0d2cf41fa052
Lucas Gabriel <gabif.johann@gmail.com>
2024-11-10 16:45:12 -0300

    feat: save note at sqlite3 db

---
+++ db.js
@@ -0,0 +1,27 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./notes.db");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS notes (
+      id TEXT PRIMARY KEY,
+      content TEXT,
+      opened_at DATE DEFAULT null,
+      created_at DATE DEFAULT (datetime('now', 'localtime'))
+    )`);
+});
+
+const saveNote = (id, content) =>
+  new Promise((resolve, reject) =>
+    db.run(
+      `
+    INSERT INTO notes (id, content) VALUES (?, ?) 
+  `,
+      [id, content],
+      (err) => (err ? reject(err) : resolve())
+    )
+  );
+
+module.exports = {
+  saveNote,
+};
--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require("express");
+const { saveNote } = require("./db");
 const app = express();
 
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/note", (req, res) => {
+app.get("/note/:id", (req, res) => {
   res.sendFile(__dirname + "/public/note.html");
+});
+
+app.post("/notes", async (req, res) => {
+  const { content } = req.body;
+
+  if (!content) {
+    return res.sendFile("<span>Erro inesperado!</span>");
+  }
+
+  const id = crypto.randomUUID();
+  await saveNote(id, content);
+  res.send(`
+    <p>Compartilhe sua nota através do link
+      <br />
+      <span>${req.headers.origin}/note/${id}</span>
+    </p>
+    `);
 });
 
 app.listen(port, () => {
